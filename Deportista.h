7746ac5f5aaaf3359e5e5cbb852fdac4962b1e2c
Jeremy Duran Vargas <jereduran77@gmail.com>
2023-04-19 19:55:49 -0600

    Cambio de variables private -> protected

--- Deportista.h
+++ Deportista.h
@@ -10,7 +10,7 @@ public:
 	//que con ManejoSimpleFecha
 	virtual ~Deportista();
 	virtual string toString() = 0; //creo que aqui va = 0 en vez de const pq es la primera clase que va a tener los metodos, ya en las otras si va const
-private:
+protected:
 	string cedula;
 	string nombre;
 	string numeroTelefono;
